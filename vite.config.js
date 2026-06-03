30bc62d8ea5cb7e5c590ae725aa68f49f26b57a8
Rishi Khadiyar <rishikhadiyar@gmail.com>
2026-06-03 19:29:43 +0530

    Feat: add interactive email modal and update workshop details

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/My-Website/',
   plugins: [react()],
   esbuild: {
     jsx: 'automatic',
